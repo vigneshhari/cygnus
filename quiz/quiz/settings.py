42359a10cb1550dc6c64ddcaa7e0ec149a3024b1
Vignesh Harim <vichuhari100@gmail.com>
2016-09-18 19:15:35 +0530

    PRODUCTION MODE

--- quiz/quiz/settings.py
+++ quiz/quiz/settings.py
@@ -23,8 +23,8 @@ TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates'),
 )
 
-STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
-STATIC_URL = (os.path.join(BASE_DIR ,'static'))
+STATIC_ROOT= "quiz/staticfiles"
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
